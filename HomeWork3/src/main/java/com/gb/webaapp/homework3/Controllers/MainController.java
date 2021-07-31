a7aa83475b0b92c17ded603824c3a15af6156ba7
IgorSudar <i.v.sudar@yandex.ru>
2021-07-31 23:23:32 +0300

    Init

---
+++ HomeWork3/src/main/java/com/gb/webaapp/homework3/Controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.gb.webaapp.homework3.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping ("/info")
+    @ResponseBody
+    public String info(){
+        return "Info page";
+    }
+}
